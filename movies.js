2dd38ffeeecce863b82032bfb7558508568285df
Adrian Hollander <adrian.hollander@kellogg.northwestern.edu>
2021-02-21 16:40:42 -0600

    Final Changes

--- movies.js
+++ movies.js
@@ -1,36 +1,79 @@
-window.addEventListener('DOMContentLoaded', async function(event) {
-  let db = firebase.firestore()
-  let apiKey = 'your TMDB API key'
-  let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
-  let json = await response.json()
-  let movies = json.results
-  console.log(movies)
-  
-  for (let i=0; i<movies.length; i++) {
-    let movie = movies[i]
-    let docRef = await db.collection('watched').doc(`${movie.id}`).get()
-    let watchedMovie = docRef.data()
-    let opacityClass = ''
-    if (watchedMovie) {
-      opacityClass = 'opacity-20'
+// window.addEventListener('DOMContentLoaded', async function(event) {
+firebase.auth().onAuthStateChanged(async function(user) {
+    let db = firebase.firestore()
+
+    if (user) {
+
+        db.collection('users').doc(user.uid).set({
+          name: user.displayName,
+          email: user.email
+        })
+
+      document.querySelector('.sign-in-or-sign-out').innerHTML = `
+      <p>Welcome ${user.displayName}!</p>
+      <a href="#" class="sign-out-button text-red-500 underline bold">Sign Out</a>`
+      
+      document.querySelector('.sign-out-button').addEventListener('click', function(event) {
+        event.preventDefault()
+        firebase.auth().signOut()
+        document.location.href = `movies.html`
+      })
+
+//${firebase.auth().signOut()}
+
+    let apiKey = 'dc6ccb48696ea753a07b2b2415024cd6'
+    let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
+    let json = await response.json()
+    let movies = json.results
+    console.log(movies)
+    
+    for (let i=0; i<movies.length; i++) {
+      let movie = movies[i]
+      let docRef = await db.collection('watched').doc(`${movie.id}-${user.uid}`).get()
+      let watchedMovie = docRef.data()
+      let opacityClass = ''
+      if (watchedMovie) {
+        opacityClass = 'opacity-20'
+      }
+
+      document.querySelector('.movies').insertAdjacentHTML('beforeend', `
+        <div class="w-1/5 p-4 movie-${movie.id}-${user.uid} ${opacityClass}">
+          <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
+          <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+        </div>
+      `)
+
+      document.querySelector(`.movie-${movie.id}-${user.uid}`).addEventListener('click', async function(event) {
+        event.preventDefault()
+        let movieElement = document.querySelector(`.movie-${movie.id}-${user.uid}`)
+        movieElement.classList.add('opacity-20')
+        await db.collection('watched').doc(`${movie.id}-${user.uid}`).set({})
+      }) 
+    }
+  } else {
+
+  document.querySelector('.movies').classList.add('hidden')
+
+    // Initializes FirebaseUI Auth
+    let ui = new firebaseui.auth.AuthUI(firebase.auth())
+
+    // FirebaseUI configuration
+    let authUIConfig = {
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      signInSuccessUrl: 'movies.html'
     }
 
-    document.querySelector('.movies').insertAdjacentHTML('beforeend', `
-      <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
-        <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
-      </div>
-    `)
+  // Starts FirebaseUI Auth
+  ui.start('.sign-in-or-sign-out', authUIConfig)
 
-    document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
-      event.preventDefault()
-      let movieElement = document.querySelector(`.movie-${movie.id}`)
-      movieElement.classList.add('opacity-20')
-      await db.collection('watched').doc(`${movie.id}`).set({})
-    }) 
+  // UID: xogEBNcrUTfzkR8tTIStIRWRrXE3 
+
   }
 })
 
+  
 // Goal:   Refactor the movies application from last week, so that it supports
 //         user login and each user can have their own watchlist.
 
